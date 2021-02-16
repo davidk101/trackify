6efec78fb36da84d7e3fb295ba6ca14c59459ee0
David Kumar <david.dn.kumar@gmail.com>
2021-02-16 13:33:29 -0600

    add boilerplate code for components

---
+++ src/components/create_event_component.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+export default class CreateEvent extends Component{
+
+    render(){
+        return(
+            <div>
+                <p>Create Event component</p>
+            </div>
+        )
+    }
+}
---
+++ src/components/event_list_component.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+export default class EventList extends Component{
+
+    render(){
+        return(
+            <div>
+                <p>Event List component</p>
+            </div>
+        )
+    }
+}
