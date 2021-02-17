b9cec196be596a927153325bd260db4871391f17
David Kumar <david.dn.kumar@gmail.com>
2021-02-16 21:19:19 -0600

    add comments for further explanation

--- src/App.js
+++ src/App.js
@@ -1,3 +1,19 @@
+// The setState( ) method triggers the re-rendering process for the component with updated state and not the whole DOM - hence React is fast
+// after the introduction of React Hooks, state can now be used both in class AND functional components.
+// Components receive data from outside with props, whereas components can create and manage their own data with state
+// Props are used to pass data, whereas state is for managing data
+// Props(immutable) are used for passing data from parent component to child component wherein they are rendered
+// React app adds nodes to the Virtual DOM known as ReactDOM
+// props is always an object -> {}
+
+// functional component -> function FunctionalComponent() {
+//                              return <h1>Hello, world</h1>; }
+
+// class component -> class ClassComponent extends React.Component {
+//  render() {
+//    return <h1>Hello, world</h1>; }
+
+// both components always return JSX. THEY DIFFER IN PASSING PROPS, HANDLING STATE AND USING LIFECYCLE METHODS
 import React from 'react'
 import {BrowserRouter as Router, Route} from 'react-router-dom' // needed to connect components to URLs
 import 'bootstrap/dist/css/bootstrap.min.css'
